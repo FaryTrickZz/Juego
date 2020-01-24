3ef3a4c6af0aebe7f6ebd0b05854e94fa10e9334
Bernat <baparicio.sc@gmail.com>
2020-01-24 02:09:18 +0100

    Cambios casa

--- venv/Habilidades.py
+++ venv/Habilidades.py
@@ -1,10 +1,13 @@
+import random
+
 class Habilidades:
 
-    def __init__(self, skillname, damage, health, shield):
+    def __init__(self, skillname, damage, health, shield, stun):
         self.skillname = skillname
         self.damage = damage
         self.health = health
         self.shield = shield
+        self.stun = stun
 
     def  getSkillname(self):
         return self.skillname
@@ -21,17 +24,52 @@ class Habilidades:
     def getHealth(self):
         return self.health
 
-    def sumarHealth(self, health):
-        self.getHealth(self.getHealth()+health)
+    def setHealth(self, health):
+        self.health = health
 
     def getShield(self):
         return self.shield
 
-    def setshield(self, shield):
-        self.getShield(self.getShield()+shield)
+    def setShield(self, shield):
+        self.shield = shield
 
-    def rSylas(self,damage):
-        self.setDamage(damage)
+    def  getStun(self):
+        return self.stun
+
+    def setStun(self, stun):
+        self.stun = stun
+
+    # Modificadores de habilidad Q
+    def qSkill(self):
+        q=random.randint(0,1)
+        if champp1=="Kayn":
+            if q == 0:
+                self.setDamage(self.getDamage()*2)
+
+        elif champp1=="Sylas":
+            q = random.randint(0, 1)
+            if q==0:
+                self.setDamage(self.getDamage()*1.25)
+
+        elif champp1=="Ekko":
+            q = random.randint(0, 1)
+            if q==0:
+                self.setDamage(self.getDamage()*1.5)
+
+    #Modificadores de habilidad W
+    def wSkill(self):
+        if champp1=="Kayn":
+            self.setDamage(self.getDamage())
+
+        elif champp1=="Sylas":
+            if champp1.getHp() < 850:
+                self.setDamage(self.getDamage()*1.35)
+                self.setHealth(self.getHealth()*1.35)
+
+        elif champp1=="Ekko":
+            w = random.randint(0, 1)
+            if w==0:
+                self.setStun(1)
 
 
 
--- venv/Main.py
+++ venv/Main.py
@@ -2,22 +2,22 @@ from Personaje import Personaje
 from Habilidades import Habilidades
 from ListaPersonajes import ListaPersonajes
 import random
-from os import system, name
 
-qKayn=Habilidades("Corte Segador", 140, 0, 0)
-wKayn=Habilidades("Alcance de cuchilla", 270, 0, 0)
-eKayn=Habilidades("Paso sombrio", 0, 150, 0)
-rKayn=Habilidades("Transgresion de umbral", 350, 300, 0)
 
-qSylas=Habilidades("Azote de cadenas", 100, 0, 0)
-wSylas=Habilidades("Matarreyes", 205, 150, 0)
-eSylas=Habilidades("Fuga", 220, 0, 0)
-rSylas=Habilidades("Usurpacion", 0, 0, 0)
+qKayn=Habilidades("Corte Segador", 140, 0, 0, "-")
+wKayn=Habilidades("Alcance de cuchilla", 270, 0, 0, "-")
+eKayn=Habilidades("Paso sombrio", 0, 150, 0, "-")
+rKayn=Habilidades("Transgresion de umbral", 350, 300, 0, "-")
 
-qEkko=Habilidades("Rebobinador de tiempo", 260, 0, 0)
-wEkko=Habilidades("Convergencia paralela", 0, 0, 160)
-eEkko=Habilidades("Salto de fase", 140, 0, 0)
-rEkko=Habilidades("Cronorruptura", 450, 200, 0)
+qSylas=Habilidades("Azote de cadenas", 100, 0, 0, "-")
+wSylas=Habilidades("Matarreyes", 205, 150, 0, "-")
+eSylas=Habilidades("Fuga", 220, 0, 0, "-")
+rSylas=Habilidades("Usurpacion", 0, 0, 0, "-")
+
+qEkko=Habilidades("Rebobinador de tiempo", 260, 0, 0, "-")
+wEkko=Habilidades("Convergencia paralela", 0, 0, 160, "-")
+eEkko=Habilidades("Salto de fase", 140, 0, 0, "-")
+rEkko=Habilidades("Cronorruptura", 450, 200, 0, "-")
 
 
 Kayn=Personaje("Kayn", 2500)
@@ -79,27 +79,43 @@ while  p1.getHp() > 0 and p2.getHp() > 0:
 
     turno = 0
 
-    #Eleccion de habilidad P1
-    x=0
-    for h in p1.getHabilidades():
-        print (str(x)+" "+h.getSkillname()+ " /Damage: "+str(h.getDamage())+" /Health: "+str(h.getHealth())+" /Shield: "+str(h.getShield()))
-        x=x+1
-    habilidadp1 = int(input("Elige una habilidad: "))
+    if Stun = 1:
 
-    #Comprobacion de turno para la Ultimate
-    while habilidadp1 == 3 and  Turnop1 < 4:
-        habilidadp1 = int(input("Elige otra habilidad: "))
+        #Eleccion de habilidad P1
+        x=0
+        for h in p1.getHabilidades():
+            print (str(x)+" "+h.getSkillname()+ " /Damage: "+str(h.getDamage())+" /Health: "+str(h.getHealth())+" /Shield: "+str(h.getShield()))
+            x=x+1
+        habilidadp1 = int(input("Elige una habilidad: "))
 
-    #Comprueba si la habilidad cura
-    if p1.getHabilidades()[habilidadp1].getHealth() > 0:
-        p1.getHealth(p1.getHabilidades()[habilidadp1].getHealth())
+        #Comprobacion de turno para la Ultimate
+        while habilidadp1 == 3 and  Turnop1 < 4:
+            habilidadp1 = int(input("Elige otra habilidad: "))
 
-    # Comprueba si la habilidad proporciona escudo
-    if p1.getHabilidades()[habilidadp1].getShield() > 0:
-        p1.getHealth(p1.getHabilidades()[habilidadp1].getShield())
+        #Comprueba la habilidad elegida
+        if habilidadp1 == 0:
+            p1.getHabilidades[habilidadp1]().qSkill()
+        elif habilidadp1 == 1:
+            p1.getHabilidades[habilidadp1]().wSkill()
+        elif habilidadp1 == 2:
+            p1.getHabilidades[habilidadp1]().eSkill()
+        elif habilidadp1 == 3:
+            p1.getHabilidades[habilidadp1]().qSkill()
 
-    #Restar vida al oponente
-    p2.restarVida(p1.getHabilidades()[habilidadp1].getDamage())
+        #Comprueba si la habilidad estunea
+        if p1.getHabilidades()[habilidadp1].getStun == 1:
+            Stun = 1
+
+        #Comprueba si la habilidad cura
+        if p1.getHabilidades()[habilidadp1].getHealth() > 0:
+            p1.getHealth(p1.getHabilidades()[habilidadp1].getHealth())
+
+        # Comprueba si la habilidad proporciona escudo
+        if p1.getHabilidades()[habilidadp1].getShield() > 0:
+            p1.getHealth(p1.getHabilidades()[habilidadp1].getShield())
+
+        #Restar vida al oponente
+        p2.restarVida(p1.getHabilidades()[habilidadp1].getDamage())
 
     #Eleccion de habilidad P2
     x = 0
@@ -111,6 +127,16 @@ while  p1.getHp() > 0 and p2.getHp() > 0:
     # Comprobacion de turno para la Ultimate
     while habilidadp2 == 3 and Turnop1 < 4:
         habilidadp2 = random.randint(0, 2)
+
+    # Comprueba la habilidad elegida
+    if habilidadp1 == 0:
+        p1.getHabilidades[habilidadp1]().qSkill()
+    elif habilidadp1 == 1:
+        p1.getHabilidades[habilidadp1]().wSkill()
+    elif habilidadp1 == 2:
+        p1.getHabilidades[habilidadp1]().eSkill()
+    elif habilidadp1 == 3:
+        p1.getHabilidades[habilidadp1]().qSkill()
 
     # Comprueba si la habilidad cura
     if p2.getHabilidades()[habilidadp2].getHealth() > 0:
